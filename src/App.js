4e19f170a6f81eb7807151d11d890201c5edcbe5
Ayako <ayakosayama1987@gmail.com>
2021-02-08 15:11:54 -0800

    add json to public and read data async

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Form from './component/Form'
 
 function App() {
+
+  const [mockData, setMockData] = useState(null);
+  
+  useEffect(async()=>{
+    try {
+      const response = await fetch('http://localhost:3000/data.json',{
+        headers : { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+         }
+      })
+      const data = await response.json()
+      setMockData(data);
+    } catch(err) {
+      console.log("error:", err)
+    }
+  },[])
   return (
     <div className="App">
-      <Form />
+      <Form mockData={mockData}/>
     </div>
   );
 }
